dfb676037534a9f6a978359613ceea8845e52d0a
Md Asar <your.mdasar468@gmail.com>
2026-02-23 10:54:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddMarks from './AddMarks';
+import MarksList from './MarksList';  
+
+export default function App() {
+  return (
+    <div className='app-container'>
+      <h1>Welcome to KL University Marks Portal</h1>
+      <center><h2>Md Asar (32723)</h2></center>
+      <AddMarks />
+      <MarksList />
+    </div>
+  );
+}
